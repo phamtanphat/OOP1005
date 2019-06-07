00ac56e37af2d023384d6c4dcb2d50461238d225
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-07 14:33:19 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop1005/MainActivity.java
@@ -0,0 +1,60 @@
+package khoapham.ptp.phamtanphat.oop1005;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AndroidException;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // Dinh nghia ra 1 doi tuong:
+
+
+//        nguyenvana.ten = "Nguyen van A";
+//        nguyenvana.tuoi = "10";
+//        nguyenvana.setNgonngu("Tieng anh");
+//
+//        Person nguyenvanb = new Person();
+//        nguyenvanb.ten = "Nguyen van B";
+//        nguyenvanb.tuoi = "20";
+//        nguyenvanb.setNgonngu("Tieng viet");
+//
+        //access modifier : private , protected , public
+
+
+        //ctrl + P
+//        Person nguyenvana = new Person("nguyen van A" , "20" , "tieng anh");
+//
+//        Log.d("BBB",nguyenvana.getTen());
+
+//        - Lop dong vat
+//                Không được nhập số chân bé hơn 1
+//                + Meo:
+//                    -Ten: "Cat"
+//                    -Sochan : 4
+//                + Cho :
+//                    -Ten : "Dog"
+//                    -So chan : 4
+//        - Lop chim
+//                Kích thước không được  < 1
+//                +Cò:
+//                    -Màu lông : "Đỏ"
+//                    -Kích thước : 50
+//                +Gõ kiến:
+//                    -Màu lông : "Đen"
+//                    -Kích thước : 15
+//        -Lớp giangvien:
+//                phuong thức hiển thị tên và giờ làm việc của giảng viên bộ môn
+//                +Android:
+//                    -Tên:"Pham tan phat";
+//                    -Giờ làm : "234567"
+//                +ios:
+//                    -Tên : "Thầy khoa"
+//                    -Giờ làm : "234567"
+
+    }
+}
